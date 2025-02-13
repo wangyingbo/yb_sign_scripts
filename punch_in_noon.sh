787f595b8dff6ea338b5a44627867ad38940cfa9
wangyingbo <wangyingbo528@126.com>
2025-02-13 09:54:46 +0800

    add punch_in.sh

---
+++ punch_in_noon.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# author: yingbo 2025.02.13
+
+
+
+# bark参数文档：https://bark.day.app/#/tutorial
+
+
+# 王迎博
+curl "https://api.day.app/mg6Tt9Kc4ygnEcULyApRTG/请记得打卡?url=dingtalk://dingtalkclient/page/link"
