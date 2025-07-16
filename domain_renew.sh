89c8243adba3054766ea49d91d732c600ff85071
wangyingbo <wangyingbo528@126.com>
2025-07-16 18:15:13 +0800

    update domain_renew

--- domain_renew.sh
+++ domain_renew.sh
@@ -9,6 +9,8 @@
 # http://152.70.160.189:28683/14yhl9t
 # http://130.61.54.30:23835/14yhl9t
 
+# 可参考 https://github.com/yonggekkk/acme-yg/blob/main/acme.sh 脚本里的 acmerenew 函数，配置服务器定时执行任务，实现定时手动续期
 
 # 王迎博
 curl "https://api.day.app/mg6Tt9Kc4ygnEcULyApRTG/请记得xui和sb脚本域名证书续期?url=safari://"
+
