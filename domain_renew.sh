218843b32207d87603f84acf06289dac434ac213
wangyingbo <wangyingbo528@126.com>
2025-07-16 17:51:43 +0800

    add domain renew

---
+++ domain_renew.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# author: yingbo 2025.07.16
+
+
+
+# bark参数文档：https://bark.day.app/#/tutorial
+
+# http://152.67.228.255:41628/14yhl9t
+# http://152.70.160.189:28683/14yhl9t
+# http://130.61.54.30:23835/14yhl9t
+
+
+# 王迎博
+curl "https://api.day.app/mg6Tt9Kc4ygnEcULyApRTG/请记得xui和sb脚本域名证书续期?url=safari://"
